8b7725a6be305ab3235bdf4602e68328fde441b0
Minh <minhkieu702@gmail.com>
2023-06-17 00:56:28 +0700

    EX17

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Contact></Contact>
+    </div>
+  );
+}
+
+export default App;
