3ee0f4753982905194c21eb2f5b78eea0c2e7aab
Minh <minhkieu702@gmail.com>
2023-06-16 18:47:48 +0700

    EX16

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddUser from './components/AddUser';
+import User from './components/User';
+
+function App() {
+  return (
+    <div>
+<AddUser></AddUser>
+<User></User>
+    </div>
+  );
+}
+
+export default App;
